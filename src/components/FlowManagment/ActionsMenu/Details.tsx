e7dee205d81f5dbce42d76ae7daeca6879535e4e
Yaryna Mazuryk <yarynam@Yarynas-Laptop.local>
2023-12-21 18:38:10 +0200

    EL-2538 add created flow details

--- src/components/FlowManagment/ActionsMenu/Details.tsx
+++ src/components/FlowManagment/ActionsMenu/Details.tsx
@@ -3,12 +3,19 @@ import { Divider, Stack, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 
 import { FULL_DATE_TIME_FORMAT } from '@constants/common';
+import { IFlowListItem } from '@domain/flow';
 
-const Details: React.FC<{ editedBy: string; editedOn: string }> = ({
-  editedBy,
-  editedOn
+const Details: React.FC<{ flow: IFlowListItem }> = ({
+  flow: { editedBy, editedOn, createdBy, createdOn }
 }) => (
   <Stack pl={2} pr={2} pt={1}>
+    <Divider />
+    <Typography pt={1} variant="caption">
+      Created by {createdBy}
+    </Typography>
+    <Typography pb={1} sx={{ fontWeight: 500 }} variant="caption">
+      {dayjs(createdOn).format(FULL_DATE_TIME_FORMAT)}
+    </Typography>
     <Divider />
     <Typography pt={1} variant="caption">
       Last edited by {editedBy}
