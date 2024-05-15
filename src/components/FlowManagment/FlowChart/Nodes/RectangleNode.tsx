574e6c5baa7261c097d3246952712623f3d64c60
Yaryna Mazuryk <yarynam@Yarynas-Laptop.fritz.box>
2024-05-15 17:59:12 +0200

    EL-4001: Add icon to subflow

--- src/components/FlowManagment/FlowChart/Nodes/RectangleNode.tsx
+++ src/components/FlowManagment/FlowChart/Nodes/RectangleNode.tsx
@@ -6,6 +6,7 @@ import styles from './style.module.scss';
 
 import CalculatorIcon from '@icons/calculator.svg';
 import BlocksIcon from '@icons/blocks.svg';
+import LineChartDotsSquareIcon from '@icons/lineChartDotsSquare.svg';
 import { NodeData, DecisionTableData } from '@domain/flow';
 import { NO_TAG_LABEL } from '@constants/common';
 import { theme } from '@theme';
@@ -21,6 +22,12 @@ const RectangleNode: React.FC<NodeProps<NodeData & DecisionTableData>> = ({
       )}
       {data.stepType === StepType.DECISION_TABLE && (
         <BlocksIcon color={theme.palette.primary.main} />
+      )}
+      {data.stepType === StepType.DECISION_TABLE && (
+        <BlocksIcon color={theme.palette.primary.main} />
+      )}
+      {data.stepType === StepType.SUBFLOW && (
+        <LineChartDotsSquareIcon color={theme.palette.primary.main} />
       )}
       <div>
         <p className={styles['node-tag']}>{data?.tag || NO_TAG_LABEL}</p>
