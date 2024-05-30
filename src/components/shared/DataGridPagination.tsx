506b6a90d4f1d53962fd7810261243fd0d685d5e
Yaryna Mazuryk <yarynam@Yarynas-Laptop.local>
2024-05-30 18:44:35 +0300

    EL-4836: Performance of Lead request report

--- src/components/shared/DataGridPagination.tsx
+++ src/components/shared/DataGridPagination.tsx
@@ -1,4 +1,5 @@
-import { Stack, Typography, Select, MenuItem, Pagination } from '@mui/material';
+import React, { useCallback, useState, useEffect } from 'react';
+import { Stack, Typography, Pagination, TextField } from '@mui/material';
 import {
   useGridApiContext,
   useGridSelector,
@@ -10,6 +11,49 @@ const DataGridPagination = () => {
   const apiRef = useGridApiContext();
   const page = useGridSelector(apiRef, gridPageSelector);
   const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+  const [inputValue, setInputValue] = useState<number | string>(page + 1);
+
+  useEffect(() => {
+    setInputValue(page + 1);
+  }, [page]);
+
+  const handlePaginationChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    apiRef.current.setPage(value - 1);
+  };
+
+  const handlePageByInput = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      const newPage = Number(value) - 1;
+
+      if (!isNaN(newPage) && newPage >= 0 && newPage < pageCount) {
+        setInputValue(value);
+        apiRef.current.setPage(newPage);
+      } else if (value === '') {
+        setInputValue(value);
+      }
+    },
+    [apiRef, pageCount]
+  );
+
+  const handleBlur = () => {
+    const newPage = Number(inputValue) - 1;
+
+    if (isNaN(newPage) || newPage < 0 || newPage >= pageCount) {
+      setInputValue(page + 1); // Reset to current page if out of range
+    } else {
+      apiRef.current.setPage(newPage);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleBlur();
+    }
+  };
 
   return (
     <Stack
@@ -21,28 +65,22 @@ const DataGridPagination = () => {
         <Typography variant="h6" pr={1}>
           Go to
         </Typography>
-        <Select
-          value={page + 1}
-          onChange={(event) => {
-            apiRef.current.setPage(+event.target.value - 1);
-          }}
+        <TextField
+          type="number"
+          value={inputValue}
+          sx={{ borderRadius: '8px', maxWidth: '64px', mr: 1 }}
           size="small"
-          displayEmpty
-          inputProps={{ 'aria-label': ' Go to page' }}
-        >
-          {Array.from({ length: pageCount }, (_, i) => (
-            <MenuItem key={i + 1} value={i + 1}>
-              {i + 1}
-            </MenuItem>
-          ))}
-        </Select>
+          onChange={handlePageByInput}
+          onKeyDown={handleKeyDown}
+          onBlur={handleBlur}
+        />
       </Stack>
       <Pagination
         sx={(theme) => ({ padding: theme.spacing(1.5, 0) })}
         color="primary"
         count={pageCount}
         page={page + 1}
-        onChange={(_event, value) => apiRef.current.setPage(value - 1)}
+        onChange={handlePaginationChange}
       />
     </Stack>
   );
