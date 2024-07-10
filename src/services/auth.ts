7106da75e4499ba280a7a0008a3ebbc88a8ef4aa
Krystyna Bodnar <bodnar.krystyna@globaldev.tech>
2024-07-10 15:48:21 +0300

    [ELMS-1410] upd onTokenChange

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,7 +3,7 @@ import { Auth } from '@eloanwarehouse/frontend-core';
 import { router } from '../routes.tsx';
 
 import { CLIENT_ID, SCOPES_LIST } from '@constants/common.tsx';
-import { api } from '@utils/api.ts';
+import { api, integrationApi, reportApi } from '@utils/api.ts';
 import { authApiBaseUrl } from '@constants/api-urls.ts';
 import routes from '@constants/routes.ts';
 
@@ -15,8 +15,12 @@ export const authService = new Auth.IAMAuthService({
   onTokenChange(token: string | null) {
     if (token) {
       api.defaults.headers.Authorization = `Bearer ${token}`;
+      reportApi.defaults.headers.Authorization = `Bearer ${token}`;
+      integrationApi.defaults.headers.Authorization = `Bearer ${token}`;
     } else {
       delete api.defaults.headers.Authorization;
+      delete reportApi.defaults.headers.Authorization;
+      delete integrationApi.defaults.headers.Authorization;
     }
   },
   onLogin() {
