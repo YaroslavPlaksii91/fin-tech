a1028288ae56c68b901d0c3359c128be1ca472a0
Yaryna Mazuryk <yarynam@Yarynas-Laptop.local>
2024-08-08 11:49:09 +0300

    EL-5658: Clean code

--- src/components/FlowManagment/FlowChart/Nodes/RectangleNode.tsx
+++ src/components/FlowManagment/FlowChart/Nodes/RectangleNode.tsx
@@ -3,21 +3,6 @@ import { Handle, Position, NodeProps } from 'reactflow';
 import { NodeData, DecisionTableData } from '@domain/flow';
 import { NO_TAG_LABEL, STEP_ICONS } from '@constants/common';
 
-// const RectangleNode: React.FC<NodeProps<NodeData & DecisionTableData>> = ({
-//   data
-// }) => (
-//   <div id={data.stepId} className={styles['node-container']}>
-//     <Handle type="source" position={Position.Right} />
-//     <div className={styles['node-header']}>
-//       {STEP_ICONS[data.stepType]}
-//       <div>
-//         <p className={styles['node-tag']}>{data?.tag || NO_TAG_LABEL}</p>
-//         <p className={styles['node-label']}>{data.name}</p>
-//       </div>
-//     </div>
-//     <Handle type="target" position={Position.Left} />
-//   </div>
-// );
 const RectangleNode: React.FC<NodeProps<NodeData & DecisionTableData>> = ({
   data
 }) => (
