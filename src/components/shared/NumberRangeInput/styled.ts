74fe74bebb8b2e460d34b5aedc560026f87a1b2d
Yaryna Mazuryk <yarynam@Yarynas-Laptop.local>
2024-08-15 13:08:40 +0300

    Fix styles for Champion chanllenger, calculation

--- src/components/shared/NumberRangeInput/styled.ts
+++ src/components/shared/NumberRangeInput/styled.ts
@@ -2,16 +2,15 @@ import { styled } from '@mui/material/styles';
 import { InputAdornment, TextField } from '@mui/material';
 
 export const StyledTextField = styled(TextField)(({ theme }) => ({
-  ...theme.typography.body2,
-  width: '111px',
   '& .MuiInputBase-root': {
     borderRadius: '6px',
-    padding: '0 8px'
+    padding: '0'
   },
   '& .MuiOutlinedInput-notchedOutline': {
     border: `1px solid ${theme.palette.action.active}`
   },
   '& .MuiOutlinedInput-input': {
+    ...theme.typography.body2,
     padding: '2.5px 2px',
     textAlign: 'center',
     '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
