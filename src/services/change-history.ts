b88e73189e89916f0fa776950303cbe4594e82e6
Yaryna Mazuryk <yarynam@Yarynas-Laptop.local>
2024-08-29 15:53:55 +0300

    Remove useless components

--- src/services/change-history.ts
+++ src/services/change-history.ts
@@ -1,18 +1,14 @@
 import { api } from '@utils/api.ts';
-import { apiUrls } from '@constants/api-urls.ts';
 import { ChangeHistoryRecord } from '@domain/changeHistory.ts';
 
 class ChangeHistoryService {
   async getList(pageNumber: number = 1, pageSize: number = 10) {
-    const { data } = await api.get<ChangeHistoryRecord[]>(
-      apiUrls.changeHistory.list,
-      {
-        params: {
-          pageNumber,
-          pageSize
-        }
+    const { data } = await api.get<ChangeHistoryRecord[]>('/change-history', {
+      params: {
+        pageNumber,
+        pageSize
       }
-    );
+    });
     return data;
   }
 }
