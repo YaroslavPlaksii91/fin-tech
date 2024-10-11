8a0157c35faf183815f67a88c6e6a24214179f50
oleksandr.andriichyshen <oleksandr.andriichyshen@globaldev.tech>
2024-10-11 16:51:13 +0200

    EL-6842 fix wrong field for leadPrice value

--- src/pages/LeadRequestsReports/utils.ts
+++ src/pages/LeadRequestsReports/utils.ts
@@ -23,7 +23,7 @@ export const getFormattedRows = (data: LeadRequestsReport[]) => {
       [COLUMN_IDS.leadProvider]: leadRequest.leadProviderId ?? '-',
       [COLUMN_IDS.leadCampaign]: leadRequest.campaignId ?? '-',
       [COLUMN_IDS.customerId]: leadResponse?.customerId ?? '-',
-      [COLUMN_IDS.leadPrice]: leadResponse?.leadPrice ?? '-',
+      [COLUMN_IDS.leadPrice]: leadRequest?.leadPrice ?? '-',
       [COLUMN_IDS.affiliate]: leadRequest.affiliateId ?? '-',
       [COLUMN_IDS.requestDate]: processingMetadata?.executionEndDateTimeUtc
         ? dayjs(processingMetadata.executionEndDateTimeUtc).format(
