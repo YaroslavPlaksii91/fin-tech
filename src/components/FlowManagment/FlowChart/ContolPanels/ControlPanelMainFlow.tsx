34b43308f7b6a05b6772e0ab271762d29da486b8
oleksandr.andriichyshen <oleksandr.andriichyshen@globaldev.tech>
2024-10-28 10:43:04 +0100

    EL-6388 add ability to push to production in view mode

--- src/components/FlowManagment/FlowChart/ContolPanels/ControlPanelMainFlow.tsx
+++ src/components/FlowManagment/FlowChart/ContolPanels/ControlPanelMainFlow.tsx
@@ -17,6 +17,7 @@ import {
   SnackbarErrorMessage,
   SnackbarMessage
 } from '@components/shared/Snackbar/SnackbarMessage';
+import Dialog from '@components/shared/Modals/Dialog';
 import { SNACK_TYPE } from '@constants/common';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
 import { saveFlow } from '@store/flow/asyncThunk';
@@ -29,14 +30,16 @@ import { updateFlowListItem } from '@store/flowList';
 import { useHasUserPermission } from '@hooks/useHasUserPermission';
 import routes from '@constants/routes';
 
-const ControlPanelMainFlow: React.FC<ControlPanelProps> = ({
+const ControlPanelMainFlow = ({
   rfInstance,
   flow,
   isDirty,
   setCopyFlow,
   isViewMode
-}) => {
-  const [loading, setLoading] = useState<boolean>(false);
+}: ControlPanelProps) => {
+  const [loading, setLoading] = useState(false);
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+
   const dispatch = useAppDispatch();
   const flowData = useAppSelector(selectFlowData);
   const user = useAppSelector(selectUserInfo);
@@ -49,85 +52,87 @@ const ControlPanelMainFlow: React.FC<ControlPanelProps> = ({
 
   const canUpdateFlow = useHasUserPermission(permissionsMap.canUpdateFlow);
 
+  const handleWarningModalOpen = () => setIsWarningModalOpen(true);
+
+  const handleWarningModalClose = () => setIsWarningModalOpen(false);
+
   const onSave = useCallback(async () => {
-    if (rfInstance && flow) {
-      try {
-        setLoading(true);
-        const formattedData = formatFlowOnSave({
-          flow: {
-            ...flow,
-            data: { ...flowData, editedBy: username }
-          },
-          rfInstance
-        });
-        const resultAction = await dispatch(saveFlow(formattedData));
-        const savedFlow = unwrapResult(resultAction);
-        setCopyFlow(cloneDeep(savedFlow));
-        dispatch(updateFlowListItem({ ...savedFlow.data, id: savedFlow.id }));
+    try {
+      setLoading(true);
+      const formattedData = formatFlowOnSave({
+        flow: {
+          ...flow,
+          data: { ...flowData, editedBy: username }
+        },
+        rfInstance
+      });
+      const resultAction = await dispatch(saveFlow(formattedData));
+      const savedFlow = unwrapResult(resultAction);
+      setCopyFlow(cloneDeep(savedFlow));
+      dispatch(updateFlowListItem({ ...savedFlow.data, id: savedFlow.id }));
 
-        enqueueSnackbar(
-          <SnackbarMessage
-            message="Success"
-            details={`Changes for the "${savedFlow.data.name}" flow were successfully saved.`}
-          />,
-          { variant: SNACK_TYPE.SUCCESS }
-        );
-      } catch (error) {
-        enqueueSnackbar(
-          <SnackbarErrorMessage message="Error" error={error} />,
-          {
-            variant: SNACK_TYPE.ERROR
-          }
-        );
-      } finally {
-        setLoading(false);
-      }
+      enqueueSnackbar(
+        <SnackbarMessage
+          message="Success"
+          details={`Changes for the "${savedFlow.data.name}" flow were successfully saved.`}
+        />,
+        { variant: SNACK_TYPE.SUCCESS }
+      );
+    } catch (error) {
+      enqueueSnackbar(<SnackbarErrorMessage message="Error" error={error} />, {
+        variant: SNACK_TYPE.ERROR
+      });
+    } finally {
+      setLoading(false);
     }
   }, [rfInstance, flow, flowData]);
 
-  const onPushFlow = useCallback(async () => {
-    if (rfInstance && flow) {
-      try {
-        setLoading(true);
-        const formattedData = formatFlowOnSave({
-          flow: { ...flow, data: flowData },
-          rfInstance
-        });
-        const resultAction = await dispatch(
-          pushProductionFlow({
-            flow: formattedData,
-            params: { pushedBy: username, note: '' }
-          })
-        );
-        const pushedFlow = unwrapResult(resultAction);
+  const handlePushToProduction = useCallback(async () => {
+    try {
+      setLoading(true);
+      const formattedData = formatFlowOnSave({
+        flow: { ...flow, data: flowData },
+        rfInstance
+      });
+      const resultAction = await dispatch(
+        pushProductionFlow({
+          flow: formattedData,
+          params: { pushedBy: username, note: '' }
+        })
+      );
+      const pushedFlow = unwrapResult(resultAction);
 
-        enqueueSnackbar(
-          <SnackbarMessage
-            message="Success"
-            details={`"${pushedFlow.data.name}" flow is published into the production successfully.`}
-          />,
-          { variant: SNACK_TYPE.SUCCESS }
-        );
-      } catch (error) {
-        const errors = parseFlowValidationErrors(
-          error,
-          rfInstance.toObject().nodes
-        );
-        enqueueSnackbar(
-          <SnackbarErrorMessage
-            message="Error"
-            parsedErrors={errors}
-            error={error}
-          />,
-          {
-            variant: SNACK_TYPE.ERROR
-          }
-        );
-      } finally {
-        setLoading(false);
-      }
+      enqueueSnackbar(
+        <SnackbarMessage
+          message="Success"
+          details={`"${pushedFlow.data.name}" flow is published into the production successfully.`}
+        />,
+        { variant: SNACK_TYPE.SUCCESS }
+      );
+    } catch (error) {
+      const errors = parseFlowValidationErrors(
+        error,
+        rfInstance.toObject().nodes
+      );
+      enqueueSnackbar(
+        <SnackbarErrorMessage
+          message="Error"
+          parsedErrors={errors}
+          error={error}
+        />,
+        {
+          variant: SNACK_TYPE.ERROR
+        }
+      );
+    } finally {
+      setLoading(false);
     }
   }, [rfInstance, flow, flowData]);
+
+  const handleConfirm = async () => {
+    await handlePushToProduction();
+    setIsWarningModalOpen(false);
+  };
 
   return (
     <StyledPanel position="top-right">
@@ -138,44 +143,54 @@ const ControlPanelMainFlow: React.FC<ControlPanelProps> = ({
         <Typography variant="h4">{flowData.name}</Typography>
       </Box>
       <Stack spacing={1} direction="row" justifyContent="flex-end">
-        {isViewMode ? (
-          !isProductionFlow &&
-          canUpdateFlow && (
-            <Button
-              sx={{ marginLeft: 'auto' }}
-              variant="contained"
-              size="small"
-              component={NavLink}
-              to={routes.underwriting.flow.edit(flow.id)}
-            >
-              Edit Flow
-            </Button>
-          )
-        ) : (
-          <>
-            {canUpdateFlow && (
-              <Button
-                size="small"
-                variant="outlined"
-                onClick={onSave}
-                disabled={loading}
-              >
-                Save changes
-              </Button>
-            )}
-            {canPushFlowToProduction && (
-              <Button
-                size="small"
-                variant="contained"
-                disabled={isDirty}
-                onClick={onPushFlow}
-              >
-                Push changes
-              </Button>
-            )}
-          </>
-        )}
+        {!isProductionFlow && canUpdateFlow && isViewMode ? (
+          <Button
+            sx={{ marginLeft: 'auto' }}
+            variant="contained"
+            size="small"
+            component={NavLink}
+            to={routes.underwriting.flow.edit(flow.id)}
+          >
+            Edit Flow
+          </Button>
+        ) : null}
+        {canUpdateFlow && !isViewMode ? (
+          <Button
+            size="small"
+            variant="outlined"
+            onClick={onSave}
+            disabled={loading}
+          >
+            Save changes
+          </Button>
+        ) : null}
+        {canPushFlowToProduction && !isProductionFlow ? (
+          <Button
+            size="small"
+            variant="contained"
+            disabled={isDirty}
+            onClick={
+              isViewMode ? handleWarningModalOpen : handlePushToProduction
+            }
+          >
+            Push To Production
+          </Button>
+        ) : null}
       </Stack>
+      <Dialog
+        title="Push Flow"
+        open={isWarningModalOpen}
+        onConfirm={handleConfirm}
+        onClose={handleWarningModalClose}
+        confirmText="Confirm"
+        confirmLoading={loading}
+        cancelText="Cancel"
+      >
+        <Typography sx={{ maxWidth: '416px' }} variant="body2">
+          This action will push the flow <b>{flow.data.name}</b> into
+          production. To confirm, please click <b>Confirm</b> button.
+        </Typography>
+      </Dialog>
     </StyledPanel>
   );
 };
