4d4a6179cdfd542741d5756678257a37cfa758fd
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-11-06 15:01:37 +0200

    EL-7114: Changes font weight in textarea

--- src/components/shared/Forms/styled.ts
+++ src/components/shared/Forms/styled.ts
@@ -5,7 +5,6 @@ import InfoIcon from '@mui/icons-material/Info';
 export const StyledTextarea = styled(TextareaAutosize)(({ theme }) => ({
   fontFamily: 'Inter, sans-serif',
   fontSize: '14px',
-  fontWeight: 600,
   boxSizing: 'border-box',
   borderRadius: '8px',
   border: `1px solid ${theme.palette.gray}`,
